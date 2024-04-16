15d82090811cabfb62f4dd5a9b1a8e6da30c7faf
Your Name <hoangthanhgolle@gmail.com.vn>
2024-04-16 19:57:52 +0700

    tao du an moi

---
+++ src/main/java/com/example/webbansach_BE/WebbansachBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.webbansach_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbansachBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbansachBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webbansach_BE/WebbansachBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webbansach_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbansachBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
