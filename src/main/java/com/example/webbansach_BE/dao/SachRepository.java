3311c17cd27f0490c1e1fcad65b861de5f2a80c7
Your Name <hoangthanhgolle@gmail.com.vn>
2024-04-20 19:16:24 +0700

    tim kiem

--- src/main/java/com/example/webbansach_BE/dao/SachRepository.java
+++ src/main/java/com/example/webbansach_BE/dao/SachRepository.java
@@ -1,9 +1,18 @@
 package com.example.webbansach_BE.dao;
 
 import com.example.webbansach_BE.entity.Sach;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestResource(path = "sach")
 public interface SachRepository extends JpaRepository<Sach, Integer> {
+        //tim kiem
+        Page<Sach> findByTenSachContaining(@RequestParam("tenSach") String tenSach, Pageable pageable);
+
+        Page<Sach> findByDanhSachTheLoai_MaTheLoai(@RequestParam("maTheLoai") int maTheLoai, Pageable pageable);
+        Page<Sach> findByTenSachContainingAndDanhSachTheLoai_MaTheLoai(@RequestParam("tenSach") String tenSach,@RequestParam("maTheLoai") int maTheLoai, Pageable pageable);
+
 }
